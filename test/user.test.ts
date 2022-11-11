527792b650d03425f981fee607e7b5d8c79c3c1f
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-11 16:55:55 -0300

    Create class Either to manager error and success result

--- test/user.test.ts
+++ test/user.test.ts
@@ -2,10 +2,11 @@ import { describe, vi, expect, test } from 'vitest'
 import { readFile } from 'fs/promises'
 import { join } from 'path'
 import { Parser } from 'xml2js'
+
+import { Either, left, right } from '@/shared/error/Either'
 interface HttpRequest {
   get: () => Promise<any>
 }
-
 
 class HttpRequestFake implements HttpRequest {
   async get (): Promise<any> {
@@ -40,21 +41,30 @@ type XmlUsers = {
 type DataUsersXml = XmlFile<XmlPagination & XmlUsers>
 
 class ServiceXmlToJson {
-  async execute<T = any>(xml: string): Promise<T | null> {
+  async execute<T = any>(xml: string): Promise<Either<XmlConvertError, T>> {
     try {
       const { parseStringPromise } = new Parser({ explicitArray: false, trim: true })
       const convertedData = await parseStringPromise(xml)
-      return convertedData
-    } catch (error) {
-      return null
+      return right(convertedData)
+    } catch (error: any) {
+      return left(new XmlConvertError(error.message))
     }
   }
 }
+
+class XmlConvertError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'XmlConvertError'
+  }
+}
+
 
 describe('#Convert user data XML', () => {
   test('Success to convert user XML to Object', async () => {
     const httpRequest = new HttpRequestFake()
     const userXml = await httpRequest.get()
+
     const expectedUser = {
       avatar: "https://cdn.fakercloud.com/avatars/al_li_128.jpg",
       createdAt: "2022-02-23T05:20:06.524Z",
@@ -65,16 +75,17 @@ describe('#Convert user data XML', () => {
     }
 
     const service = new ServiceXmlToJson()
-    const users = await service.execute<DataUsersXml>(userXml) as DataUsersXml
-    const { usersList } = users.data
+    const { value } = await service.execute<DataUsersXml>(userXml)
+    const { data } = value as DataUsersXml
+    const { usersList } = data
 
     expect(usersList.item[0]).toMatchObject(expectedUser)
   })
 
   test('Fail to convert file XML to Object', async () => {
     const service = new ServiceXmlToJson()
-    const users = await service.execute<DataUsersXml>('')
-
-    expect(users).toBeNull()
+    const data = await service.execute<DataUsersXml>(null as any)
+    
+    expect(data.value).toBeInstanceOf(XmlConvertError)
   })
 })
