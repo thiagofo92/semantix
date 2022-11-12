89d3f9ae9d66bb1e4c9ca4187134b10ba3c042ab
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-12 18:55:01 -0300

    Move files to folder memory

---
+++ src/infra/services/db/memory/person-service-memory.ts
@@ -0,0 +1,16 @@
+import { PersonEntity } from "@/core/entities/person-entity";
+import { PersonCreateError } from "@/core/errors/person-error";
+import { PersonRepository } from "@/core/repositories/person-repository";
+import { Either, left, right } from "@/shared/error/Either";
+
+export class PersonService implements PersonRepository {
+  private person: PersonEntity[] = []
+  async create(person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
+    try {
+      this.person.push(person)
+      return right(true)
+    } catch (error: any) {
+      return left(new PersonCreateError(error.message)) ;
+    }   
+  }
+}
