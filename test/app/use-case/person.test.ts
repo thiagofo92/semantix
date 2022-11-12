873c9dfe41c37cead29f8ec1630d0965efecf8b1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-12 18:46:51 -0300

    Set up test to communic with the flux

--- test/app/use-case/person.test.ts
+++ test/app/use-case/person.test.ts
@@ -1,11 +1,13 @@
 import { describe, vi, expect, test } from "vitest";
+import { PersonService } from "@/infra/services/db";
+import { PersonUseCase } from "@/app/use-case/person-use-case";
 
 describe('# Use case to create the user in Mango DB', () => {
   test('Create user in DB', async () => {
     const personService = new PersonService();
     const usecase = new PersonUseCase(personService) 
 
-    const result = await usecase.execute()
+    const result = await usecase.create()
 
     expect(result).toStrictEqual(true)
   })
