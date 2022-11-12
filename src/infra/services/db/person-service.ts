0e41d47a6493d2ea91af1558ad7c99ed9bacdb81
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-12 18:46:02 -0300

    Create the file person and config return to always return true

--- src/infra/services/db/person-service.ts
+++ src/infra/services/db/person-service.ts
@@ -1,8 +1,9 @@
+import { PersonEntity } from "@/core/entities/person-entity";
 import { PersonCreateError } from "@/core/errors/person-error";
 import { PersonRepository } from "@/core/repositories/person-repository";
 import { Either, left, right } from "@/shared/error/Either";
 
-export class PersonServices implements PersonRepository { 
+export class PersonService implements PersonRepository { 
   async create(person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
     try {
       return right(true)
