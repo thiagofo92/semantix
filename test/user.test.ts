7ae796bf7bd906b290121df3566d5ff2a15708e1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-12 18:31:36 -0300

    Create user-case to person

--- test/user.test.ts
+++ test/user.test.ts
@@ -2,6 +2,11 @@ import { describe, vi, expect, test } from "vitest";
 
 describe('# Use case to create the user in Mango DB', () => {
   test('Create user in DB', async () => {
-    
+    const personService = new PersonService();
+    const usecase = new PersonUseCase(personService) 
+
+    const result = await usecase.execute()
+
+    expect(result).toStrictEqual(true)
   })
 })
