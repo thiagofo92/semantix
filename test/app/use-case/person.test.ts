be090752ae0bb67fd98e05f8af3ec0d359986716
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-12 18:57:04 -0300

    Add test of error

--- test/app/use-case/person.test.ts
+++ test/app/use-case/person.test.ts
@@ -1,14 +1,25 @@
 import { describe, vi, expect, test } from "vitest";
-import { PersonService } from "@/infra/services/db";
-import { PersonUseCase } from "@/app/use-case/person-use-case";
+import { PersonService } from "@/infra/services/db/memory";
+import { PersonUseCase } from "@/app/use-case";
 
 describe('# Use case to create the user in Mango DB', () => {
   test('Create user in DB', async () => {
     const personService = new PersonService();
-    const usecase = new PersonUseCase(personService) 
+    const usecase = new PersonUseCase(personService)
 
     const result = await usecase.create()
 
     expect(result).toStrictEqual(true)
+  })
+
+  test('Error when try to save data in DB', async () => {
+    const personService = new PersonService();
+
+    
+    const usecase = new PersonUseCase(personService)
+
+    const result = await usecase.create()
+
+    expect(result).toStrictEqual(false)
   })
 })
