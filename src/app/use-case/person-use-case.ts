72bbffa6d115d6f45a74456e8eae236e488f22bb
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-14 09:28:09 -0300

    Create function to format input and convert data to PersonEtity

--- src/app/use-case/person-use-case.ts
+++ src/app/use-case/person-use-case.ts
@@ -6,17 +6,41 @@ export class PersonUseCase {
   constructor (private readonly personService: PersonService) {}
 
   async create (person: PersonModel): Promise<boolean> {
-    const personEntity: PersonEntity = {
-      fullName: '',
-      email: '',
-      address: '',
-      addressNumber: 0,
-      phoneNumber: ''
-    }
+    const personEntity = this.toPersonEntity(person)
     const result = await this.personService.create(personEntity)
 
     if (result.isLeft()) return false
 
     return true
+  }
+
+  private toPersonEntity (person: PersonModel): PersonEntity {
+    const personEntity: PersonEntity = {
+      fullName: '',
+      email: '',
+      address: '',
+      addressNumber: '',
+      phoneNumber: ''
+    }
+
+    personEntity.fullName = `${person.firstName} ${person.lastName}`
+    personEntity.email = person.email
+    personEntity.address = person.address[0].street
+    personEntity.addressNumber = person.address[0].number
+    personEntity.phoneNumber = person.contact[0].phoneNumber
+
+    return personEntity
+  }
+
+  private toPersonEntityArray (person: PersonModel[]): PersonEntity[] {
+    const personEntity = person.map<PersonEntity>(item => ({
+      fullName: `${item.firstName} ${item.lastName}`,
+      email: item.email,
+      address: item.address[0].street,
+      addressNumber: item.address[0].number,
+      phoneNumber: item.contact[0].phoneNumber
+    }))
+
+    return personEntity
   }
 }
