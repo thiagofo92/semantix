436f75886c833fbef5d667b56d98c12205fb07e9
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-14 14:10:02 -0300

    Add body to be send to post and put, add options to be used if necessary

--- src/app/repositories/request-https-repository.ts
+++ src/app/repositories/request-https-repository.ts
@@ -12,9 +12,17 @@ export interface ResponseHttp<T> {
   text: string
 }
 
+type HeaderOptions = {
+  [key in string]: string
+}
+
+export interface RequestOptions {
+  headers: HeaderOptions
+}
+
 export interface RequestHttpsRepository {
-  get: <T = any>() => Promise<Either<RequestHttpsGetError, ResponseHttp<T>>>
-  post: <T = any> () => Promise<Either<RequestHttpsPostError, ResponseHttp<T>>>
-  put: <T = any> () => Promise<Either<RequestHttpsPutError, ResponseHttp<T>>>
-  del: <T = any> () => Promise<Either<RequestHttpsDelError, ResponseHttp<T>>>
+  get: <T = any>(url: string, options?: RequestOptions) => Promise<Either<RequestHttpsGetError, ResponseHttp<T>>>
+  post: <T = any> (url: string, body: any, options?: RequestOptions) => Promise<Either<RequestHttpsPostError, ResponseHttp<T>>>
+  put: <T = any> (url: string, body: any, options?: RequestOptions) => Promise<Either<RequestHttpsPutError, ResponseHttp<T>>>
+  del: <T = any> (url: string, options?: RequestOptions) => Promise<Either<RequestHttpsDelError, ResponseHttp<T>>>
 }
