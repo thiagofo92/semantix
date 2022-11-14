7ae304f1338ee6bea15f9aeca1f4360a88bc8af5
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-14 11:15:24 -0300

    Refactory - add parameter type and return Either type

--- src/app/repositories/xml-to-json-repository.ts
+++ src/app/repositories/xml-to-json-repository.ts
@@ -1,3 +1,6 @@
+import { XmlConvertError } from '@/core/errors/convert-error'
+import { Either } from '@/shared/error/Either'
+
 export interface XmlToJsonRepository {
-  execute: () => Promise<any>
+  execute: <T = any>(xml: string) => Promise<Either<XmlConvertError, T>>
 }
