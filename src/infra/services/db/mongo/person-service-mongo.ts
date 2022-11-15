b0b5c92d7cbfcda358ab2e3dca3f82370624c034
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 00:07:11 -0300

    Start to create file that will create and findall person from mongo

--- src/infra/services/db/mongo/person-service-mongo.ts
+++ src/infra/services/db/mongo/person-service-mongo.ts
@@ -1,8 +1,22 @@
 import { PersonRepository } from '@/app/repositories'
 import { PersonEntity } from '@/core/entities/person-entity'
-import { PersonCreateError } from '@/app/errors'
-import { Either } from '@/shared/error/Either'
+import { PersonCreateError, PersonFindAllError } from '@/app/errors'
+import { Either, left, right } from '@/shared/error/Either'
 
 export class PersonServiceMongo implements PersonRepository {
-  create: (person: PersonEntity) => Promise<Either<PersonCreateError, boolean>>
+  async create (person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
+    try {
+      return right(true)
+    } catch (error: any) {
+      return left(new PersonCreateError(error.message))
+    }
+  }
+
+  async findAll (): Promise<Either<PersonFindAllError, PersonEntity[]>> {
+    try {
+      return right([])
+    } catch (error: any) {
+      return left(new PersonFindAllError(error.message))
+    }
+  }
 }
