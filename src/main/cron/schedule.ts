5625149eb8d8c2cd28efe2df07311a0748ae80c3
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 00:29:26 -0300

    Get users from xml url

--- src/main/cron/schedule.ts
+++ src/main/cron/schedule.ts
@@ -1,3 +1,22 @@
-// import { RequestHttps } from '@/infra/services/http/request-https'
+import 'dotenv/config'
+import { PersonUseCase } from '@/app/use-case'
+import { XmlUseCase } from '@/app/use-case/xml-use-case'
+import { XmlToJsonService } from '@/infra/services/convert/xml-to-json'
+import { PersonService } from '@/infra/services/db/memory'
+import { RequestHttps } from '@/infra/services/http/request-https'
 
-// const requestHttps = new RequestHttps()
+void (async () => {
+  const requestHttps = new RequestHttps()
+  const xmlServices = new XmlToJsonService()
+  const personService = new PersonService()
+  const personUseCase = new PersonUseCase(personService)
+
+  const user = process.env.API_TECH_USER || ''
+  const password = process.env.API_TECH_PASSWORD || ''
+  const xmlUseCase = new XmlUseCase(requestHttps, xmlServices, user, password)
+  const baseUrl = process.env.API_TECH_BASEURL || ''
+  const url = `${baseUrl}/users`
+
+  const users = await xmlUseCase.getUsers({ url, page: '10', limit: '10' })
+  console.log(users)
+})()
