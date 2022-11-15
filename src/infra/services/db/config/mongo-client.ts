4640878361fdebfe7139d2d8475fe2e182a259d4
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 11:27:10 -0300

    Fix the parameters to connect in MongoDB

--- src/infra/services/db/config/mongo-client.ts
+++ src/infra/services/db/config/mongo-client.ts
@@ -3,18 +3,21 @@ import mongoose from 'mongoose'
 
 const dbName = process.env.DB_NAME || ''
 const dbHost = process.env.DB_HOST || ''
-const dbPort = process.env.DB_PORT || ''
+const dbPort = process.env.DB_PORT || '27017'
 const dbUser = process.env.DB_USER || ''
 const dbPassword = process.env.DB_PASSWORD || ''
 
 let MongoClient: null | typeof mongoose = null
 
-export async function MongoConnection (): Promise<any> {
+export async function MongoConnection (): Promise<typeof mongoose> {
   try {
     if (MongoClient) return MongoClient
+    console.log(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`)
 
     MongoClient = await mongoose
-      .connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}?retryWrites=true&w=majority`)
+      .connect(`mongodb://${dbUser}:${dbPassword}@${dbHost}:${dbPort}`, {
+        dbName
+      })
 
     return MongoClient
   } catch (error: any) {
