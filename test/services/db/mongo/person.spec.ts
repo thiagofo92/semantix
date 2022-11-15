d166cf251f8b61516ec7f6c452e00142f731076a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 12:16:39 -0300

    Add fail test to create and findall

--- test/services/db/mongo/person.spec.ts
+++ test/services/db/mongo/person.spec.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import { PersonServiceMongo } from '@/infra/services/db/mongo'
 import { personEntityMock } from '@test/data/mock/entity/person-entity-mock'
-import { describe, test, expect, beforeAll, afterAll, afterEach } from 'vitest'
+import { describe, test, expect, beforeAll, afterAll, afterEach, vi } from 'vitest'
 import { MongoConnection } from '@/infra/services/db/config/mongo-client'
 import { PersonModel } from '@/infra/services/db/mongo/schema'
+import { left } from '@/shared/error/Either'
+import { PersonCreateError, PersonFindAllError } from '@/app/errors'
 
 describe('# Create person in database', () => {
   beforeAll(async () => {
@@ -40,5 +42,22 @@ describe('# Create person in database', () => {
     if (users.isLeft()) throw Error('Findall person in mongo')
 
     expect(users.value[0]).toMatchObject(personEntity)
+  })
+
+  test('Faitl to create a person in Mongo', async () => {
+    const personService = new PersonServiceMongo()
+    const personEntity = personEntityMock()
+    vi.spyOn(personService, 'create').mockResolvedValueOnce(left(new PersonCreateError('Test create fail fake')))
+    const result = await personService.create(personEntity)
+
+    expect(result.value).instanceOf(PersonCreateError)
+  })
+
+  test('Fail get all person from Mongo', async () => {
+    const personService = new PersonServiceMongo()
+    vi.spyOn(personService, 'findAll').mockResolvedValueOnce(left(new PersonFindAllError('Test findall fail fake')))
+    const result = await personService.findAll()
+
+    expect(result.value).toStrictEqual(PersonFindAllError)
   })
 })
