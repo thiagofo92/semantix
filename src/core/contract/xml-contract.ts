e161251ef4c4480c9947c8aa198a748f8f103ed1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-14 22:21:04 -0300

    Refactory add new methods and params

--- src/core/contract/xml-contract.ts
+++ src/core/contract/xml-contract.ts
@@ -1,3 +1,22 @@
-export interface XmlUseCaseContract {
-  get: () => Promise<string>
+import {
+  DataUsersXmlEntity,
+  DataUsersAddressXmlEntity,
+  DataUsersContactXmlEntity
+} from '../entities/xml-entity'
+
+export interface Login {
+  user: string
+  password: string
+}
+
+export interface Users {
+  url: string
+  pagination: string
+  limit: string
+}
+
+export interface XmlDataUsersUseCaseContract {
+  getUsers: (user: Users) => Promise<DataUsersXmlEntity>
+  getAddress: (url: string) => Promise<DataUsersAddressXmlEntity>
+  getContact: (url: string) => Promise<DataUsersContactXmlEntity>
 }
