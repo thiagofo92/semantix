02fbb7812c35856d34406cf9324fe7a93874a5bf
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 14:05:34 -0300

    Refactory PersonServiceMemory to implements new methods

--- src/infra/services/db/memory/person-service-memory.ts
+++ src/infra/services/db/memory/person-service-memory.ts
@@ -1,9 +1,26 @@
 import { PersonEntity } from '@/core/entities/person-entity'
-import { PersonCreateError } from '@/app/errors/person-error'
+import { PersonCreateError, PersonFindAllError } from '@/app/errors/person-error'
 import { PersonRepository } from '@/app/repositories/person-repository'
 import { Either, left, right } from '@/shared/error/Either'
 
 export class PersonService implements PersonRepository {
+  async createMany (person: PersonEntity[]): Promise<Either<PersonCreateError, boolean>> {
+    try {
+      this.person.push(...person)
+      return right(true)
+    } catch (error: any) {
+      return left(new PersonCreateError(error.message))
+    }
+  }
+
+  async findAll (): Promise<Either<PersonFindAllError, PersonEntity[]>> {
+    try {
+      return right(this.person)
+    } catch (error: any) {
+      return left(new PersonCreateError(error.message))
+    }
+  }
+
   private readonly person: PersonEntity[] = []
   async createOne (person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
     try {
