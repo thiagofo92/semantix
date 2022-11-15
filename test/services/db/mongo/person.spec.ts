ef7dfd25ead2702a015f60769d9ed125d036acbd
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 11:41:44 -0300

    Fail when try to get all person from db

--- test/services/db/mongo/person.spec.ts
+++ test/services/db/mongo/person.spec.ts
@@ -1,18 +1,24 @@
 import 'dotenv/config'
 import { PersonServiceMongo } from '@/infra/services/db/mongo'
 import { personEntityMock } from '@test/data/mock/entity/person-entity-mock'
-import { describe, test, expect, beforeAll, afterAll } from 'vitest'
+import { describe, test, expect, beforeAll, afterAll, afterEach } from 'vitest'
 import { MongoConnection } from '@/infra/services/db/config/mongo-client'
+import { PersonModel } from '@/infra/services/db/mongo/schema'
 
 describe('# Create person in database', () => {
   beforeAll(async () => {
     await MongoConnection()
   })
 
+  afterEach(async () => {
+    await PersonModel.deleteMany()
+  })
+
   afterAll(async () => {
     const connection = await MongoConnection()
     await connection.disconnect()
   })
+
   test('Success to create a person in Mongo', async () => {
     const personService = new PersonServiceMongo()
     const personEntity = personEntityMock()
@@ -20,5 +26,19 @@ describe('# Create person in database', () => {
 
     if (result.isLeft()) throw Error('Create person in mongo')
     expect(result.value).toStrictEqual(true)
+  })
+
+  test('Success to findall a person in Mongo', async () => {
+    const personService = new PersonServiceMongo()
+    const personEntity = personEntityMock()
+
+    const created = await personService.create(personEntity)
+    if (created.isLeft()) throw Error('Create person in mongo')
+
+    const users = await personService.findAll()
+
+    if (users.isLeft()) throw Error('Findall person in mongo')
+
+    expect(users.value[0]).toMatchObject(personEntity)
   })
 })
