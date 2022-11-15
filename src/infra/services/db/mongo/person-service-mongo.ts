40da2fbdd2508f1dccb5629dd07caabae3a8bf76
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 11:32:17 -0300

    Create person entity in Mongo DB

--- src/infra/services/db/mongo/person-service-mongo.ts
+++ src/infra/services/db/mongo/person-service-mongo.ts
@@ -2,10 +2,15 @@ import { PersonRepository } from '@/app/repositories'
 import { PersonEntity } from '@/core/entities/person-entity'
 import { PersonCreateError, PersonFindAllError } from '@/app/errors'
 import { Either, left, right } from '@/shared/error/Either'
+import { UsersModel } from './schema'
 
 export class PersonServiceMongo implements PersonRepository {
   async create (person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
     try {
+      await UsersModel.create({
+        ...person
+      })
+
       return right(true)
     } catch (error: any) {
       return left(new PersonCreateError(error.message))
