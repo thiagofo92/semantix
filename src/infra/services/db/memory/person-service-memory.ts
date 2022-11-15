9265a604e77402524ec2ca571fdaad22663780ef
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 14:26:10 -0300

    Refactory - add ID to entity person when use createOne and createMany in PersonServiceMemory

--- src/infra/services/db/memory/person-service-memory.ts
+++ src/infra/services/db/memory/person-service-memory.ts
@@ -2,11 +2,16 @@ import { PersonEntity } from '@/core/entities/person-entity'
 import { PersonCreateError, PersonFindAllError } from '@/app/errors/person-error'
 import { PersonRepository } from '@/app/repositories/person-repository'
 import { Either, left, right } from '@/shared/error/Either'
+import { faker } from '@faker-js/faker'
 
 export class PersonService implements PersonRepository {
   async createMany (person: PersonEntity[]): Promise<Either<PersonCreateError, boolean>> {
     try {
-      this.person.push(...person)
+      const tmp = person.map<PersonEntity>(item => ({
+        ...item,
+        id: faker.datatype.uuid()
+      }))
+      this.person.push(...tmp)
       return right(true)
     } catch (error: any) {
       return left(new PersonCreateError(error.message))
@@ -24,7 +29,10 @@ export class PersonService implements PersonRepository {
   private readonly person: PersonEntity[] = []
   async createOne (person: PersonEntity): Promise<Either<PersonCreateError, boolean>> {
     try {
-      this.person.push(person)
+      this.person.push({
+        ...person,
+        id: faker.datatype.uuid()
+      })
       return right(true)
     } catch (error: any) {
       return left(new PersonCreateError(error.message))
