7bdc55617d899a300532e7e2f3cc2b2f54b016c5
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 11:27:52 -0300

    Add beForeAll and AfterAll to connect in Mongo and Disconnect from mongo

--- test/services/db/mongo/person.spec.ts
+++ test/services/db/mongo/person.spec.ts
@@ -1,8 +1,18 @@
+import 'dotenv/config'
 import { PersonServiceMongo } from '@/infra/services/db/mongo'
 import { personEntityMock } from '@test/data/mock/entity/person-entity-mock'
-import { describe, test, expect } from 'vitest'
+import { describe, test, expect, beforeAll, afterAll } from 'vitest'
+import { MongoConnection } from '@/infra/services/db/config/mongo-client'
 
 describe('# Create person in database', () => {
+  beforeAll(async () => {
+    await MongoConnection()
+  })
+
+  afterAll(async () => {
+    const connection = await MongoConnection()
+    await connection.disconnect()
+  })
   test('Success to create a person in Mongo', async () => {
     const personService = new PersonServiceMongo()
     const personEntity = personEntityMock()
