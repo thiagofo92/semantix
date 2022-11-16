5f89c2338546110243bbe140a84a200a145a592d
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 09:43:29 -0300

    Router file

---
+++ src/main/routes/routers.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from 'express'
+import { ExpressAdapter } from '../adapter'
+import { factoryFileController, factoryFolderController } from '../factories/controllers'
+
+export class Routers {
+  constructor (private readonly router: Router) {}
+
+  private folder (): void {
+    const controller = factoryFolderController()
+    this.router.post('/folder/create', ExpressAdapter(controller.create))
+  }
+
+  private file (): void {
+    const controller = factoryFileController()
+    this.router.post('/file/create', ExpressAdapter(controller.create))
+    this.router.delete('/file/delete', ExpressAdapter(controller.create))
+  }
+
+  build (): Router {
+    this.folder()
+    this.file()
+
+    return this.router
+  }
+}
