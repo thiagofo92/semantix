a0739c7f8725ffc2314a145e965e1479b9c0ab95
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 23:32:33 -0300

    Change type of return to ResponseModel

--- src/app/repositories/folder-repository.ts
+++ src/app/repositories/folder-repository.ts
@@ -4,5 +4,5 @@ import { FolderCreateError, FolderFindByIdError } from '../errors/folder-error'
 
 export interface FolderRepository {
   create: (folder: FolderEntity) => Promise<Either<FolderCreateError, boolean>>
-  findById: (name: string) => Promise<Either<FolderFindByIdError, { idFolder: string }>>
+  findByName: (name: string) => Promise<Either<FolderFindByIdError, { idFolder: string }>>
 }
