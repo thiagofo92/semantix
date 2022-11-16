a957a8dbc59257f9040530066ff46be2d4eaa54a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 09:44:28 -0300

    File controller

--- src/app/controllers/file-controller.ts
+++ src/app/controllers/file-controller.ts
@@ -1,11 +1,13 @@
+import { FileUseCaseContract } from '@/core/contract/file-contract'
 import { RequestContract, ResponseContract } from '@/main/contract'
 
-export class FolderController {
-  constructor () {}
+export class FileController {
+  constructor (
+    private readonly fileUseCase: FileUseCaseContract
+  ) {}
+
   async create ({ body }: RequestContract<any>): Promise<ResponseContract> {
-    return {
-      data: '',
-      statusCode: 201
-    }
+    const result = await this.fileUseCase.create(body)
+    return result
   }
 }
