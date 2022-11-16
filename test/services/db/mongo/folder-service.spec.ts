ff9e86d4dd96aae42c586157a019c7705a4180e8
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 21:32:20 -0300

    Test create data in collection folder

--- test/services/db/mongo/folder-service.spec.ts
+++ test/services/db/mongo/folder-service.spec.ts
@@ -2,7 +2,7 @@ import { describe, vi, test, expect } from 'vitest'
 
 describe('# Folder Tes', () => {
   test('Create folder', async () => {
-    const folderService = new FolderService()
+    const folderService = new FolderServiceMongo()
     const result = await folderService.create('test')
     expect(result).toStrictEqual(true)
   })
