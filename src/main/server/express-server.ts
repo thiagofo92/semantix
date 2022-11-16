7f3a1b795fb0c1f619f4fc9af61b98316666006a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 10:04:29 -0300

    Add routes to express

--- src/main/server/express-server.ts
+++ src/main/server/express-server.ts
@@ -1,14 +1,16 @@
 import Express from 'express'
 import { Routers } from '../routes/routers'
+import configMiddleware from '../middlewares/config'
 
 const app = Express()
 
 const host = '0.0.0.0'
 const port = 4500
-const route = Express.Router()
-const routers = new Routers(route)
-route.use('/api', routers.build())
-
+const router = Express.Router()
+const routers = new Routers(router)
+router.use('/api', routers.build())
+router.use(configMiddleware)
+app.use(router)
 app.listen(port, host, () => {
   console.log(`Server is on ${host}:${port}`)
 })
