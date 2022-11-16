de3efe26e29a35b4fe280c1ca2e860e59b52942b
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 15:12:01 -0300

    Add method to delete one file

--- src/app/use-case/file-use-case.ts
+++ src/app/use-case/file-use-case.ts
@@ -9,6 +9,8 @@ import { FileRepository } from '../repositories/file-repository'
 import { FolderRepository } from '../repositories/folder-repository'
 import { goFile } from '../util/go-file'
 import FormData from 'form-data'
+import { GO_FILE_TOKEN } from '../controllers/token'
+import qs from 'qs'
 
 export class FileUseCase implements FileUseCaseContract {
   constructor (
@@ -18,26 +20,28 @@ export class FileUseCase implements FileUseCaseContract {
   ) {}
 
   async create (fileUpload: FileUploadModel): Promise<ResponseModel> {
-    const result = await this.folderService.findByName(fileUpload.name)
+    const result = await this.folderService.findByName(fileUpload.folderName)
 
     if (result.isLeft()) return internalError(result.value.message)
 
-    if (!result.value) return notFound('Folder name not found')
+    if (!result.value) return notFound('Not found folder name')
 
-    const getServerStore = await this.requestHttps.get<GoFileResponseServerEntity>('')
+    const getServerStore = await this.requestHttps.get<GoFileResponseServerEntity>(goFile.urlGetServer)
 
     if (getServerStore.isLeft()) return internalError(getServerStore.value.message)
 
     const serverStore = getServerStore.value.data.data.server
-    const url = `http://${serverStore}.${goFile.urlUploadFile}`
+    const url = `https://${serverStore}.${goFile.urlUploadFile}`
     const formData = new FormData()
     formData.append('token', fileUpload.token)
     formData.append('folderId', result.value.idFolder)
-    formData.append('file', fileUpload.file)
+    formData.append('file', fileUpload.file, {
+      filename: fileUpload.fileName
+    })
 
     const options = {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        ...formData.getHeaders()
       }
     }
 
@@ -58,6 +62,27 @@ export class FileUseCase implements FileUseCaseContract {
   }
 
   async del (name: string): Promise<ResponseModel> {
+    const file = await this.fileService.findByName(name)
+
+    if (!file) return notFound(`File ${name} not found`)
+
+    if (file.isLeft()) return internalError(file.value)
+
+    const deleteFile = {
+      token: GO_FILE_TOKEN,
+      contentsId: file.value?.idFile
+    }
+
+    const data = qs.stringify(deleteFile)
+    const options = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+    const response = await this.requestHttps.del(goFile.urlDeleteContent, data, options)
+
+    if (response.isLeft()) return internalError(response.value)
+
     return success(true)
   }
 }
