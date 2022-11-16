c06e47a269273a8b0d578a581ac9585a781d18ed
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 03:48:03 -0300

    Create file to update files in GoFile

--- src/app/use-case/file-use-case.ts
+++ src/app/use-case/file-use-case.ts
@@ -1,12 +1,14 @@
 import { FileUseCaseContract } from '@/core/contract/file-contract'
 import { FileEntity } from '@/core/entities'
-import { GoFileResponseServerEntity, GoFileUploadEntity } from '@/core/entities/go-file-entity'
+import { GoFileResponseServerEntity, GoFileResponseUploadEntity } from '@/core/entities/go-file-entity'
 import { internalError, notFound, success, successToCreate } from '../helpers/response-helpers'
 import { FileUploadModel } from '../models/file-model'
 import { ResponseModel } from '../presenters/model/response-model'
 import { RequestHttpsRepository } from '../repositories'
 import { FileRepository } from '../repositories/file-repository'
 import { FolderRepository } from '../repositories/folder-repository'
+import { goFile } from '../util/go-file'
+import FormData from 'form-data'
 
 export class FileUseCase implements FileUseCaseContract {
   constructor (
@@ -22,22 +24,37 @@ export class FileUseCase implements FileUseCaseContract {
 
     if (!result.value) return notFound('Folder name not found')
 
-    const response = await this.requestHttps.get<GoFileResponseServerEntity>('')
+    const getServerStore = await this.requestHttps.get<GoFileResponseServerEntity>('')
 
-    if (response.isLeft()) return internalError(response.value.message)
+    if (getServerStore.isLeft()) return internalError(getServerStore.value.message)
 
-    const serverStore = response.value.data.data.server
-    const goFileUpload: GoFileUploadEntity = {
-      token: fileUpload.token,
-      folderId: result.value.idFolder,
-      file: fileUpload.file
+    const serverStore = getServerStore.value.data.data.server
+    const url = `http://${serverStore}.${goFile.urlUploadFile}`
+    const formData = new FormData()
+    formData.append('token', fileUpload.token)
+    formData.append('folderId', result.value.idFolder)
+    formData.append('file', fileUpload.file)
+
+    const options = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
     }
+
+    const responseUploadFile = await this.requestHttps.post<GoFileResponseUploadEntity>(url, formData, options)
+
+    if (responseUploadFile.isLeft()) return internalError(responseUploadFile.value.message)
 
     const fileEntity: FileEntity = {
-
+      idFile: responseUploadFile.value.data.data.fileId,
+      idFolder: result.value.idFolder,
+      name: responseUploadFile.value.data.data.fileName
     }
 
-    return successToCreate(true)
+    const file = await this.fileService.create(fileEntity)
+
+    if (file.isLeft()) return internalError(file.value.message)
+    return successToCreate(file.value)
   }
 
   async del (name: string): Promise<ResponseModel> {
