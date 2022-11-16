2e6b7384fbd57df57869ccdcfde0a290c04b28de
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 15:09:49 -0300

    Add method to delete one folder

--- src/app/controllers/folder-controller.ts
+++ src/app/controllers/folder-controller.ts
@@ -1,13 +1,35 @@
 import { FolderUseCaseContract } from '@/core/contract'
 import { RequestContract, ResponseContract } from '@/main/contract'
+import { FolderModel } from '../models'
+import { GO_FILE_TOKEN } from './token'
 
+interface FolderCreate {
+  name: string
+  parentFolderId: string
+}
+
+interface FolderDelete {
+  folderName: string
+}
 export class FolderController {
   constructor (
     private readonly folderUseCase: FolderUseCaseContract
-  ) {}
+  ) {
+    this.create = this.create.bind(this)
+    this.del = this.del.bind(this)
+  }
 
-  async create ({ body }: RequestContract<any>): Promise<ResponseContract> {
-    const result = await this.folderUseCase.create(body)
+  async create ({ body }: RequestContract<FolderCreate>): Promise<ResponseContract> {
+    const folderModel: FolderModel = {
+      ...body,
+      token: GO_FILE_TOKEN
+    }
+    const result = await this.folderUseCase.create(folderModel)
+    return result
+  }
+
+  async del ({ query }: RequestContract<FolderDelete>): Promise<ResponseContract> {
+    const result = await this.folderUseCase.del(query.folderName)
     return result
   }
 }
