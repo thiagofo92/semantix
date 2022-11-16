0fe02bfa9bccac92a6a3e03ba6162531267368a6
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 15:12:56 -0300

    Add url to delete contents by id

--- src/app/util/go-file.ts
+++ src/app/util/go-file.ts
@@ -1,5 +1,6 @@
 export const goFile = {
-  urlCreateFolder: 'https://api.gofile.io/createFodler',
+  urlCreateFolder: 'https://api.gofile.io/createFolder',
   urlGetServer: 'https://api.gofile.io/getServer',
-  urlUploadFile: 'gofile.io/uploadFile'
+  urlUploadFile: 'gofile.io/uploadFile',
+  urlDeleteContent: 'https://api.gofile.io/deleteContent'
 }
