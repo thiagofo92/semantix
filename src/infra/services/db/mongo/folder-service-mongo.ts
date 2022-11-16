88e6beeb8c02c1be9cb6910e3d090ef8ad9d844d
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 21:56:50 -0300

    Create test to folder service mongo

--- src/infra/services/db/mongo/folder-service-mongo.ts
+++ src/infra/services/db/mongo/folder-service-mongo.ts
@@ -1,19 +1,29 @@
 import { FolderCreateError, FolderFindByIdError } from '@/app/errors/folder-error'
 import { FolderRepository } from '@/app/repositories/folder-repository'
 import { Either, left, right } from '@/shared/error/Either'
+import { FolderModel } from './schema'
 
 export class FolderServiceMongo implements FolderRepository {
-  async create (name: string): Promise<Either<FolderCreateError, boolean>> {
+  async create (idFolder: string, name: string): Promise<Either<FolderCreateError, boolean>> {
     try {
+      await FolderModel.create({
+        id_folder: idFolder,
+        name
+      })
       return right(true)
     } catch (error: any) {
       return left(new FolderCreateError(error.message))
     }
   }
 
-  async findById (name: string): Promise<Either<FolderFindByIdError, { idfolder: string }>> {
+  async findById (name: string): Promise<Either<FolderFindByIdError, { idFolder: string }>> {
     try {
-      return right(true)
+      const folder = await FolderModel.findOne({
+        name
+      })
+      return right({
+        idFolder: folder ? folder.id_folder : ''
+      })
     } catch (error: any) {
       return left(new FolderFindByIdError(error.message))
     }
