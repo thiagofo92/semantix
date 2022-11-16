127c5e40d02b0b6fdde9521bd47b76d0a28a3d7c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 22:54:49 -0300

    Start to create test to use case folder

--- test/app/use-case/folder.spec.ts
+++ test/app/use-case/folder.spec.ts
@@ -1,1 +1,24 @@
-import 
+import { FolderServiceMemory } from '@test/services/mock/db/memory/folder-service-memory'
+import { describe, vi, test, expect } from 'vitest'
+
+interface Factory {
+  sut: FolderUseCase
+  folderServiceMemory: FolderServiceMemory
+}
+
+function factoryFolderUseCase () {
+  const folderServiceMemory = new FolderServiceMemory()
+  const sut = new FolderUseCase(folderServiceMemory)
+  return {
+    sut,
+    folderServiceMemory
+  }
+}
+
+describe('# Folder use case', () => {
+  test('Success to create folder', async () => {
+    const { sut } = factoryFolderUseCase()
+    const result = await sut.create()
+    expect(result).toStrictEqual(true)
+  })
+})
