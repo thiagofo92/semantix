14fb1be46aa33c227b24a16a93d832c7e65fccb0
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 02:36:20 -0300

    Create file to test FileUseCase

---
+++ test/app/use-case/file-use-case.spec.ts
@@ -0,0 +1,44 @@
+import { describe, test, expect, vi } from 'vitest'
+import { fileModelMock } from '@test/data/mock/model'
+import { FileServiceMemory, FolderServiceMemory } from '@test/services/mock/db/memory'
+import { RequesHttpsGoFileFake } from '@test/services/mock/request-https-gofile-fake'
+import { FileUseCase } from '@/app/use-case/file-use-case'
+import { FileCreateError } from '@/app/errors/file-error'
+import { left } from '@/shared/error/Either'
+
+interface Factory {
+  sut: FileUseCase
+  fileServiceMemory: FileServiceMemory
+  folderServiceMemory: FolderServiceMemory
+}
+
+function factoryFileUseCase (): Factory {
+  const fileServiceMemory = new FileServiceMemory()
+  const folderServiceMemory = new FolderServiceMemory()
+  const requestHttps = new RequesHttpsGoFileFake()
+  const sut = new FileUseCase(fileServiceMemory, folderServiceMemory, requestHttps)
+  return {
+    sut,
+    fileServiceMemory,
+    folderServiceMemory
+  }
+}
+
+describe('Test file use case', () => {
+  test('Success to create file', async () => {
+    const { sut } = factoryFileUseCase()
+    const fileModel = fileModelMock()
+    const result = await sut.create(fileModel)
+
+    expect(result.data).toStrictEqual(true)
+  })
+
+  test('Error to create file', async () => {
+    const { sut, fileServiceMemory, folderServiceMemory } = factoryFileUseCase()
+    const fileModel = fileModelMock()
+    vi.spyOn(folderServiceMemory, 'findByName').mockResolvedValueOnce(left(new FileCreateError('Test')))
+    const result = await sut.create(fileModel)
+
+    expect(result.statusCode).toStrictEqual(500)
+  })
+})
