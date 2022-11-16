82e643c6d1fdf19f2d497c4237534597fe474a67
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 15:16:13 -0300

    Create file express

--- src/main/server/express-server.ts
+++ src/main/server/express-server.ts
@@ -1,16 +1,25 @@
+import 'dotenv/config'
 import Express from 'express'
 import { Routers } from '../routes/routers'
 import configMiddleware from '../middlewares/config'
+import { MongoConnection } from '@/infra/services/db/config/mongo-client'
+void (async () => {
+  try {
+    await MongoConnection()
+    const app = Express()
+    const host = '0.0.0.0'
+    const port = 4500
+    const router = Express.Router()
+    router.use(Express.json())
+    router.use(configMiddleware)
 
-const app = Express()
-
-const host = '0.0.0.0'
-const port = 4500
-const router = Express.Router()
-const routers = new Routers(router)
-router.use('/api', routers.build())
-router.use(configMiddleware)
-app.use(router)
-app.listen(port, host, () => {
-  console.log(`Server is on ${host}:${port}`)
-})
+    const routers = new Routers(router)
+    router.use('/api', routers.build())
+    app.use(router)
+    app.listen(port, host, () => {
+      console.log(`Server is on ${host}:${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+})()
