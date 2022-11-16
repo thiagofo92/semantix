746b731a3802ff40767401a14f8ab520b8ad6537
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-15 22:55:11 -0300

    Start to create test to use case folder

---
+++ test/services/mock/db/memory/folder-service-memory.ts
@@ -0,0 +1,28 @@
+import { FolderCreateError, FolderFindByIdError } from '@/app/errors/folder-error'
+import { FolderRepository } from '@/app/repositories/folder-repository'
+import { FolderEntity } from '@/core/entities'
+import { Either, left, right } from '@/shared/error/Either'
+
+export class FolderServiceMemory implements FolderRepository {
+  private readonly folder: FolderEntity [] = []
+  async create (folder: FolderEntity): Promise<Either<FolderCreateError, boolean>> {
+    try {
+      this.folder.push(folder)
+      return right(true)
+    } catch (error: any) {
+      return left(new FolderCreateError('Folder Service memory'))
+    }
+  }
+
+  async findById (name: string): Promise<Either<FolderFindByIdError, { idFolder: string }>> {
+    try {
+      const result = this.folder.find(item => item.name === name)
+
+      return right({
+        idFolder: result ? result.idFolder : ''
+      })
+    } catch (error: any) {
+      return left(new FolderFindByIdError('Folder Service memory'))
+    }
+  }
+}
