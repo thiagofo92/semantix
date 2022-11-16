f0cd8ed76ff5f0c262bfd8d97ff345ce7761a10c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-16 09:44:48 -0300

    Folder controller route

--- src/app/controllers/folder-controller.ts
+++ src/app/controllers/folder-controller.ts
@@ -1,3 +1,13 @@
-export class FolderController {
+import { FolderUseCaseContract } from '@/core/contract'
+import { RequestContract, ResponseContract } from '@/main/contract'
 
+export class FolderController {
+  constructor (
+    private readonly folderUseCase: FolderUseCaseContract
+  ) {}
+
+  async create ({ body }: RequestContract<any>): Promise<ResponseContract> {
+    const result = await this.folderUseCase.create(body)
+    return result
+  }
 }
